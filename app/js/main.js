f6ba73e619e78f58faf8d0689f2d9b667d5cc94d
khasenovweb <hasenovkirill93@gmail.com>
2020-03-01 18:20:38 +0500

    dlfkdlfk

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+ $(document).ready(function(){
+ 	$('.nav__links a').click(function(){
+ 		event.preventDefault();
+ 		var type = $(this).attr('data-type');
+
+ 		if(type === 'all') {
+ 			$('.item').show();
+ 		}else {
+ 			$('.item').hide();
+ 			$('.item[data-type="'+type+'"]').show();
+ 		}
+ 		$('.nav__links a').removeClass('active');
+ 		$(this).addClass('active');
+ 	});
+ });
